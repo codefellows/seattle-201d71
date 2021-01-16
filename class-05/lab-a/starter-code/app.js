095a820b0257c7894e3c6513e8a8a9243caa065c
nacerillo <nicholascerillo@gmail.com>
2021-01-16 13:25:00 -0800

    Problem 3 completed

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -49,10 +49,14 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+var sum = a + b + c;
+var prod = a * b * c;
+return [sum, prod]
+
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+sumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
