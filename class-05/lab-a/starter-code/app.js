05d4729cd96219a3a906b962ca67a7bcc23c67af
don <bandy@gmail.com>
2021-01-15 19:09:32 -0800

    finished problem 3.  Learned A LOT on this problem!

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -14,12 +14,13 @@ function sum(a, b) { //eslint-disable-line
   var sum = a + b;
   
   result[0] = sum;
-  result[1] = `The sum of ${a} and ${b} is ${result}.`
+  result[1] = `The sum of ${a} and ${b} is ${result}.` 
   return result;
 }
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -48,12 +49,6 @@ testMultiply(5,9);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 
-
-
-
-
-
-
 /////////////////////////////////////
 /* Problem 3
 Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
@@ -67,13 +62,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var result = [];
+  result[0] = sum(a, sum(b, c)[0])[0]
+  result[1] = multiply(a, multiply(b, c)[0])[0];
+  result[2] = `${a} and ${b} and ${c} sum to ${result[0]}.`
+  result[3] = `The product of ${a} and ${b} and ${c} is ${result[1]}.`
+  return result;
 }
-
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
+
+
+
 
 /////////////////////////////////////
 /* Problem 4
