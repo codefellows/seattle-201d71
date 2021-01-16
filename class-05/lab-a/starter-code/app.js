d3be856fc99141d274791e6b586f681741425d7d
codeslayer-787 <irwin_ayala@yahoo.com>
2021-01-16 14:59:36 -0600

    solved the sum problem

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,12 +8,18 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+var a = 4;
+var b = 7;
+var sum = a + b;
 
-}
+function testSum(){ //eslint-disable-line
+//    for (var i=0; i < a.length; i++){
+  console.log('The sum of ' + a + ' and ' + b + ' is: ' + sum);
+  }
+
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum();
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
