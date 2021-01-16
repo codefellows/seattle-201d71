c7267d82356c41e5872ce43ef5ff07d7444037e0
don <bandy@gmail.com>
2021-01-15 16:27:26 -0800

    finished problem 1

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -1,6 +1,6 @@
 'use strict';
 /////////////////////////////////////
-/* Problem 1 (this is your demo that we'll solve in class)
+/*Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The sum of 4 and 7 is 11."
@@ -9,11 +9,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+  var result = [];
+  
+  var sum = a + b;
+  
+  result[0] = sum;
+  result[1] = `The sum of ${a} and ${b} is ${result}.`
+  return result;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
