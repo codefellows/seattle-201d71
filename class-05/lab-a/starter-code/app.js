213e2a803d07717e6f6beadfd321ed4273305f88
nacerillo <nicholascerillo@gmail.com>
2021-01-19 09:38:32 -0800

    Making Changes to Lab-a, corrected functions so that they follow the instructions

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -9,7 +9,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function testSum(a, b) { //eslint-disable-line
-    return a + b; 
+var sum = a + b;
+var sum_string = `The sum of ${a} and ${b} is ${sum}`
+var returnSumArray = [sum,sum_string];
+    return  returnSumArray; 
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -27,7 +30,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-        return a * b;
+        var prod = a * b;
+        var prod_string = `The product of ${a} and ${b} is ${prod}`
+        var returnMultArray = [prod,prod_string];
+            return  returnMultArray; 
 }
 multiply(5,9);
 // Here is the test for multiply(); uncomment it to run it
@@ -48,12 +54,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-var sumx = testSum(a,b);
-var sum_final = testSum(sumx,c);
+var sumx = testSum(a,b)[0];
+var sum_final = testSum(sumx,c)[0];
 
-var prodx = multiply(a,b);
-var prod_final = multiply(prodx,c);
-return [sum_final, prod_final];
+var prodx = multiply(a,b)[0];
+var prod_final = multiply(prodx,c)[0];
+
+var mult_string = `The product of ${a}, ${b}, and ${c} is ${prod_final}`
+var sum_string = `The sum of ${a}, ${b}, and ${c} is ${sum_final}`
+return [sum_final, prod_final, mult_string, sum_string];
 
 }
 
@@ -76,10 +85,9 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-    var sum_X = testSum(su)
-    var finalsum = sumArr[0] + sumArr[1] + sumArr[2];
+    var finalSum = sumAndMultiply(sumArr)[0];
     var finalstring = sumArr[0] + "," + sumArr[1] + "," + sumArr[2] + " was passed in as an array of numbers, and " + finalsum + " is the sum.";
-    return [finalsum, finalstring];   
+    return [finalSum, finalstring];   
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -101,8 +109,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var multArray = [2,3,4];
 function multiplyArray(multArr) { //eslint-disable-line
-        var prod = multiply(multArr[0],multArr[1]);
-        var prod_final = multiply(prod,multArr[2]);
+        var prod = multiply(multArr[0],multArr[1])[0];
+        var prod_final = multiply(prod,multArr[2])[0];
         multstring = multArr.toString();
         var multstring = "The numbers " + multstring + " have a product of " + prod_final;
         return [prod, multstring];       
@@ -136,13 +144,13 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
         var product = 1;
         var dynamicString = dynamicArray.toString();
         for(var x = 0; x < dynamicArray.length; x++){
-                product = multiply(product, dynamicArray[x]);
+                product = multiply(product, dynamicArray[x])[0];
         }
         var finalstring = "The numbers " + dynamicString + " have the product " + product;
         return finalstring;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
- testMultiplyAnyArray(testDynamicArray);
-
+ //testMultiplyAnyArray(testDynamicArray);
+multiplyAnyArray(dynaicArray);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
